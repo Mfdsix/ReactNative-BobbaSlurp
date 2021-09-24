19782fc2ae2aa68f8910fe1af01b4483237cc05a
Mahfudz Ainur Rif'an <Mfdsix.1nd0@gmail.com>
2021-09-24 14:35:49 +0700

    separate history datas to source folder

---
+++ src/sources/Histories.js
@@ -0,0 +1,24 @@
+const histories = [
+  {
+    date: 'Yesterday',
+    bobba: 0.0024,
+    speed: '60 GH/s',
+  },
+  {
+    date: 'Sept 22 2021',
+    bobba: 0.002,
+    speed: '62 GH/s',
+  },
+  {
+    date: 'Sept 21 2021',
+    bobba: 0.0019,
+    speed: '59 GH/s',
+  },
+  {
+    date: 'Sept 20 2021',
+    bobba: 0.0022,
+    speed: '65 GH/s',
+  },
+];
+
+export {histories};
